24eed04b6f53a996107456ab980c67196fd7ed22
Jennifer Nissley <jennifer@nissley.de>
2019-11-06 13:18:10 +0100

    kiali graph specification can now be uploaded

--- graph.js
+++ graph.js
@@ -3,6 +3,7 @@
     kiali json-format
 */
 function upload_kiali_graph(evt) {
+    console.log("upload kiali");
     // TODO: do this with a predefined object that represents the correct format
     // so the file can be rejected if it is not correct
     var file = evt.target.files[0];
@@ -11,11 +12,16 @@ function upload_kiali_graph(evt) {
     var reader = new FileReader();
     reader.onload = function() {
         kiali_graph_string = reader.result;
-        //document.getElementById("imagetest").innerHTML = kiali_graph_string;
+        // to prevent graph from disappearing when site is reloaded
+        window.localStorage.setItem('graph', kiali_graph_string);
+        window.localStorage.setItem('graph_type', 'kiali');
         var kiali_graph_obj = JSON.parse(kiali_graph_string);
         // convert kiali format into the standard format
-        convert_kiali_to_standard(kiali_graph_obj);
+        graph_obj = convert_kiali_to_standard(kiali_graph_obj);
         draw_graph(graph_obj);
+        // TODO: here the user might be asked to provide links to his documentation
+        // because a kiali graph does not contain such information
+        document.getElementById("graph_file_kiali").value = null;
     }
     reader.readAsText(file);
 }
@@ -28,6 +34,7 @@ function upload_kiali_graph(evt) {
           Benutzer erfahren können, welches Format ihr Graph haben sollte
 */
 function upload_standard_graph(evt){
+    console.log("upload standard");
     var file = evt.target.files[0];
     var graph_string;
 
@@ -36,9 +43,10 @@ function upload_standard_graph(evt){
         graph_string = reader.result;
         // to prevent graph from disappearing when site is reloaded
         window.localStorage.setItem('graph', graph_string);
-        //document.getElementById("imagetest").innerHTML = kiali_graph_string;
+        window.localStorage.setItem('graph_type', 'standard');
         var graph_obj = JSON.parse(graph_string);
         draw_graph(graph_obj);
+        document.getElementById("graph_file").value = null;
     }
     reader.readAsText(file);
 }
@@ -51,6 +59,7 @@ function upload_standard_graph(evt){
     has to be converted before calling this function
 */
 function draw_graph(graph_obj) {
+
     // create the graph
     var g = new dagreD3.graphlib.Graph({compound:true});
     g.setGraph({});
@@ -63,7 +72,14 @@ function draw_graph(graph_obj) {
         node = nodes[i];
         // The links are not limited to swagger documentation. The user should be able to bind any link to his node using the link attribute in the graph specification.
         // This design choice enables the user to use the website with any documentation form. Each microservice can be documented in it's own way.
-        g.setNode(node.id, { labelType: "html", label: "<a href=" + node.link + ">" + node.app + "-" + node.version + "</a>",  width: node.app.length*10+10, height: 40, href: "http://www.google.com"});
+        // Setting a link to a documentation is not required
+        // If the graph specification was uploaded in kiali format, the links can be set later.
+        if(node.link !== undefined){
+            g.setNode(node.id, { labelType: "html", label: "<a href=" + node.link + ">" + node.app + "-" + node.version + "</a>",  width: node.app.length*10+10, height: 40, href: "http://www.google.com"});
+        }
+        else{
+            g.setNode(node.id, { labelType: "html", label: node.app + "-" + node.version,  width: node.app.length*10+10, height: 40, href: "http://www.google.com"});
+        }
         // create a cluster if the node is another version of an already existing app
         if(nodes_included.includes(node.app)){
             if(!clusters.includes(node.app)){
@@ -97,7 +113,10 @@ function draw_graph(graph_obj) {
 
     // draw the graph
     var render = new dagreD3.render();
-    var svg = d3.select("svg"), svgGroup = svg.append("g");
+    //d3.select("svg").remove();
+    //var svg = d3.select("#graph").append("svg").attr("width", 800).attr("height", 500);
+    var svg = d3.select("svg");
+    var svgGroup = svg.append("g");
     render(d3.select("svg g"), g);
 
     // Center the graph
@@ -109,11 +128,40 @@ function draw_graph(graph_obj) {
 
 
 /*
-    TODO 2
     convert the kiali json format into the easier format used to draw the graph
 */
+// TODO: How can the user add links to his documentation after uploading a kiali graph?
+// option 1: Make nodes clickable to add a link
+// option 2: store the standard format of the kiali graph
+//           and make it downloadable, to add the missing links
+// option 3: after drawing the graph, make a form pop up where the user
+//           is asked for a link for each node
 function convert_kiali_to_standard(kiali_graph_obj){
+    var graph_obj = {
+        nodes: [],
+        edges: []
+    };
 
+    var nodes = kiali_graph_obj.elements.nodes;
+    for(var i = 0; i < nodes.length; i++){
+        node = nodes[i];
+        graph_obj.nodes.push({
+            id : node.data.id,
+            app : node.data.app,
+            version : node.data.version
+        });
+    }
+
+    var edges = kiali_graph_obj.elements.edges;
+    for(var i = 0; i < edges.length; i++){
+        edge = edges[i];
+        graph_obj.edges.push({
+            source : edge.data.source,
+            target : edge.data.target
+        });
+    }
+
+    return graph_obj;
 }
 
 /*
@@ -138,31 +186,13 @@ document.addEventListener("DOMContentLoaded", function() {
 // to prevent graph from disappering when the site is reloaded
 window.onload = function(e){
     var graph_stored = window.localStorage.getItem('graph');
+    var graph_type = window.localStorage.getItem('graph_type');
     if(graph_stored === null){
         return;
     }
     var graph_obj = JSON.parse(graph_stored);
+    if(graph_type == 'kiali'){
+        convert_kiali_to_standard(graph_obj);
+    }
     draw_graph(graph_obj);
 }
-
-/* 
-Dieses Format wollen wir erreichen:
-{
-    "nodes": [
-        {
-          "id": "47efcb6a38cec94b8f02e15c58ce44df",
-          "app": "details",
-          "version": "v1",
-          // erstmal per Hand einfügen
-          "link" : "details-v1/docs"
-        }
-    ],
-    "edges": [
-      {
-        "id": "5d577c549824d1376e9fb8477324240c",
-        "source": "11b370489b738638fabddc4f4ce47ebd",
-        "target": "b744c74ec7104950b36f9bf0b47a22fd"
-      }
-    ]
-}
-*/
